953ed99cdb453d86091a1dc2c1eb3dfa201ab6e2
ginger <26461046+gingerchew@users.noreply.github.com>
2024-12-18 16:04:00 -0500

    Working version of the images replacement feature
    currently all images are stored in 'public' this should be configurable in the future

--- loader.ts
+++ loader.ts
@@ -1,10 +1,21 @@
 import mammoth from 'mammoth';
+import sizeOf from 'image-size';
+import { unlink, writeFile } from 'node:fs/promises';
+import { join } from 'node:path';
 import { type Loader } from 'astro/loaders';
+import * as jsdom from 'jsdom';
+import { getImage } from 'astro/assets';
 import { glob } from 'node:fs';
+
+const { JSDOM } = jsdom;
+
+type ImageConfig = Parameters<typeof getImage>[1];
 
 interface Options {
     sources: string|string[];
-    styleMap?: string|string[]
+    styleMap?: string|string[];
+    imgConfig?: ImageConfig;
+    transformOptions?: Record<string, unknown>
 }
 
 function getId(str:string):string {
@@ -15,8 +26,62 @@ function getId(str:string):string {
     return id;
 }
 
-export default function docxLoader(options: Options):Loader {
+async function parseImages(id: string, html:string, transform: Options['transformOptions'], options: ImageConfig): Promise<[string, string[]]> {
+    const dom = new JSDOM(html, { resources: 'usable' });
+    const images = dom.window.document.querySelectorAll('img');
+    
+    const imagePaths = await Promise.all(Array.from(images).map(async (img, i) => {
+        const matches = img.src.match(/^data:(.+);base64,(.+)$/)
+        if (!matches) {
+            throw new Error('Invalid Data URI');
+        }
 
+        const [,contentType, data] = matches;
+
+        const binary = Buffer.from(data, 'base64');
+        const  outputFilePath = join(import.meta.dirname, 'public', `${id}_${i}.${contentType.split('/')[1]}`);
+        await writeFile(outputFilePath, binary);
+        const dimensions:Record<string, number> = {};
+        await new Promise<void>(res => {
+            sizeOf(outputFilePath, (err, dims) => {
+                if (err) throw err;
+
+                dimensions.height = dims!.height!;
+                dimensions.width = dims!.width!;
+                res();
+            })
+        })
+        const tmp = await getImage({ src: outputFilePath, ...transform, ...dimensions }, options);
+        
+        img.src = tmp.src.replace(`${import.meta.dirname}/public`, '');
+        img.height = tmp.attributes.height;
+        img.width = tmp.attributes.width;
+        if (tmp.srcSet.attribute.length > 0) img.srcset = tmp.srcSet.attribute.replaceAll(new RegExp(`${import.meta.dirname}/public`, 'g'), '');
+        if ('loading' in tmp.attributes) img.setAttribute('loading', tmp.attributes.loading);
+        if ('decoding' in tmp.attributes) img.setAttribute('decoding', tmp.attributes.decoding);
+        img.alt = tmp.attributes.alt ?? '';
+        return outputFilePath;
+    }))
+    
+    return [dom.window.document.documentElement.outerHTML, imagePaths];
+}
+
+const defaultImageOptions = {
+    endpoint: {
+        route: ''
+    },
+    service: {
+        entrypoint: '',
+        config: {}
+    },
+    domains: [],
+    remotePatterns: [],
+    experimentalResponsiveImages: false
+} satisfies ImageConfig
+
+export default function docxLoader(options: Options):Loader {
+    options.imgConfig = options.imgConfig ?? defaultImageOptions satisfies ImageConfig;
+    options.transformOptions = options.transformOptions ?? {}
     return {
         name: 'docx-loader',
         async load({ store, generateDigest, logger }) {
@@ -36,17 +101,23 @@ export default function docxLoader(options: Options):Loader {
                                 });
                             }
                             const id = getId(match);
+                            const [parsedValue, imagePaths] = await parseImages(id, value, options.transformOptions, options.imgConfig!);
                             const data = {
                                 match,
                                 id
                             };
                             const digest = generateDigest({
                                 id,
-                                rendered: { html: value },
+                                rendered: { 
+                                    html: parsedValue,
+                                    metadata: {
+                                        imagePaths
+                                    }
+                                },
                                 data
                             });
                             store.set({
-                                rendered: { html: value },
+                                rendered: { html: parsedValue },
                                 id,
                                 digest,
                                 data
